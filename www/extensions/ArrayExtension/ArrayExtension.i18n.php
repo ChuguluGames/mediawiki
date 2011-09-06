39cb7dea006cab30ccf8bcf4df68d5684ba224f2
Maxime <127086@supinfo.com>
2011-09-06 12:43:51 +0200

    First commit

---
+++ www/extensions/ArrayExtension/ArrayExtension.i18n.php
@@ -0,0 +1,177 @@
+<?php
+
+/**
+ * Internationalization file for the ArrayExtension extension.
+ *
+ * @since 1.3.4
+ *
+ * @file ArrayExtension.i18n.php
+ * @ingroup ArrayExtension
+ *
+ * @licence MIT License
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+
+$messages = array();
+
+/** English
+ * @author Jeroen De Dauw
+ */
+$messages['en'] = array(
+	'arrayext-desc' => 'Store and compute named arrays',
+);
+
+/** Message documentation (Message documentation)
+ * @author Purodha
+ */
+$messages['qqq'] = array(
+	'arrayext-desc' => '{{desc}}',
+);
+
+/** Belarusian (Taraškievica orthography) (‪Беларуская (тарашкевіца)‬)
+ * @author EugeneZelenko
+ */
+$messages['be-tarask'] = array(
+	'arrayext-desc' => 'Захаваньне і вылічэньне масіваў з назвамі',
+);
+
+/** Breton (Brezhoneg)
+ * @author Fulup
+ */
+$messages['br'] = array(
+	'arrayext-desc' => 'Enrollañ ha plediñ gant an taolennoù anvet',
+);
+
+/** Bosnian (Bosanski)
+ * @author CERminator
+ */
+$messages['bs'] = array(
+	'arrayext-desc' => 'Sačuvaj i izračunaj imenovane nizove',
+);
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'arrayext-desc' => 'Ermöglicht das Speichern und Nutzen definierter Datenreihen',
+);
+
+/** French (Français)
+ * @author Wyz
+ */
+$messages['fr'] = array(
+	'arrayext-desc' => 'Enregistrer et traiter les tableaux nommés',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'arrayext-desc' => 'Almacenar e calcular táboas con nomes',
+);
+
+/** Swiss German (Alemannisch)
+ * @author Als-Holder
+ */
+$messages['gsw'] = array(
+	'arrayext-desc' => 'Macht s Spychere un Nutze vu definierte Datezylete megli',
+);
+
+/** Hebrew (עברית)
+ * @author Amire80
+ */
+$messages['he'] = array(
+	'arrayext-desc' => 'אחסון וחישוב של מערכים בעלי שמות',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'arrayext-desc' => 'Pomjenowane polowe wariable składować a wobličić',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'arrayext-desc' => 'Immagazinar e computar arrays nominate',
+);
+
+/** Colognian (Ripoarisch)
+ * @author Purodha
+ */
+$messages['ksh'] = array(
+	'arrayext-desc' => 'Felder met Name berääschne un faßhallde.',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'arrayext-desc' => 'Späicheren a Benotze vun definéierten Tabellen',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'arrayext-desc' => 'Складирај и пресметај именувани низи',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'arrayext-desc' => 'Benoemde verzamelingen opslaan en berekenen',
+);
+
+/** Polish (Polski)
+ * @author Sp5uhe
+ */
+$messages['pl'] = array(
+	'arrayext-desc' => 'Przechowywanie i przeliczanie nazwanych tabel',
+);
+
+/** Piedmontese (Piemontèis)
+ * @author Dragonòt
+ */
+$messages['pms'] = array(
+	'arrayext-desc' => "Memorisa e càlcola j'array nominà",
+);
+
+/** Portuguese (Português)
+ * @author Hamilton Abreu
+ */
+$messages['pt'] = array(
+	'arrayext-desc' => 'Armazenar e calcular matrizes designadas',
+);
+
+/** Russian (Русский)
+ * @author Александр Сигачёв
+ */
+$messages['ru'] = array(
+	'arrayext-desc' => 'Хранение и вычисление именованных массивов',
+);
+
+/** Swedish (Svenska)
+ * @author WikiPhoenix
+ */
+$messages['sv'] = array(
+	'arrayext-desc' => 'Lagra och beräkna namngivna arrayer',
+);
+
+/** Tagalog (Tagalog)
+ * @author AnakngAraw
+ */
+$messages['tl'] = array(
+	'arrayext-desc' => 'Itabi at tuusin ang pinangalanang mga hanay',
+);
+
+/** Veps (Vepsan kel')
+ * @author Игорь Бродский
+ */
+$messages['vep'] = array(
+	'arrayext-desc' => 'Varata da lugeda nimitadud massivad',
+);
+
---
+++ www/extensions/semantic-bundle/SemanticBundle.php
@@ -0,0 +1,15 @@
+<?php
+if ( !defined( 'MEDIAWIKI' ) ) {
+    echo "This file is not a valid entry point.";
+    exit( 1 );
+}
+
+$wgExtensionCredits[defined( 'SEMANTIC_EXTENSION_TYPE' ) ? 'semantic' : 'other'][] = array(
+	'path' => __FILE__,
+        'name' => 'Semantic Bundle',
+        'description' => 'A pre-packaged bundle of extensions meant to be used on wikis based around the Semantic MediaWiki extension.',
+	'version' => '2011-03-29.20110329',
+        'author' => array( '[http://www.mediawiki.org/wiki/User:Sergey_Chernyshev Sergey Chernyshev]', '[http://www.mediawiki.org/wiki/User:Yaron_Koren Yaron Koren]', '[http://www.mediawiki.org/wiki/User:Jeroen_De_Dauw Jeroen De Dauw]' ),
+        'url' => 'http://www.mediawiki.org/wiki/Semantic_Bundle'
+);
+
