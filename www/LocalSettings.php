631dfc5e23c12e80aedcf9896e12818606a8567a
Maxime <127086@supinfo.com>
2011-09-06 14:37:13 +0200

    fichier include extentions a part

--- www/LocalSettings.php
+++ www/LocalSettings.php
@@ -184,10 +184,5 @@ $wgHooks['AbortNewAccount'][] = 'fnAbortNonMyDomainHook';
 # Disable cache
 $wgEnableParserCache = false;
 $wgCachePages = false;
-require_once("$IP/extensions/SmbLinks.php" );
 
-
-require_once( "$IP/extensions/semantic-bundle/SemanticBundleSettings.php" );
-require_once( "$IP/extensions/semantic-bundle/SemanticBundle.php" );
-include("$IP/extensions/intersection/DynamicPageList.php");
-//require_once( "$IP/extensions/OpenID/OpenID.php" );
+require_once("$IP/extentions.php");
---
+++ www/extentions.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once("$IP/extensions/SmbLinks.php" );
+require_once( "$IP/extensions/semantic-bundle/SemanticBundleSettings.php" );
+require_once( "$IP/extensions/semantic-bundle/SemanticBundle.php" );
+include("$IP/extensions/intersection/DynamicPageList.php");
+//require_once( "$IP/extensions/OpenID/OpenID.php" );
+
+?>
