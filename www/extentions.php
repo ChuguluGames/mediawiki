fd771c2875ff232da9e3135e9bd207f8fe6252f5
Maxime <127086@supinfo.com>
2011-09-06 14:45:38 +0200

    Activation OpenID

--- www/extentions.php
+++ www/extentions.php
@@ -4,6 +4,6 @@ require_once("$IP/extensions/SmbLinks.php" );
 require_once( "$IP/extensions/semantic-bundle/SemanticBundleSettings.php" );
 require_once( "$IP/extensions/semantic-bundle/SemanticBundle.php" );
 include("$IP/extensions/intersection/DynamicPageList.php");
-//require_once( "$IP/extensions/OpenID/OpenID.php" );
+require_once( "$IP/extensions/OpenID/OpenID.php" );
 
 ?>
