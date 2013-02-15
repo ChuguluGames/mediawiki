cba4837bfe396dab18f1048bc08aa4f68a9fa6fc
Olivier Thierry <olivier.thierry42@gmail.com>
2013-02-15 16:59:44 +0100

    Created deploy branch for heroku (expect a specific project structure in order to detect application's env)

--- www/extentions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once("$IP/extensions/SmbLinks.php" );
-#require_once( "$IP/extensions/semantic-bundle/SemanticBundleSettings.php" );
-#require_once( "$IP/extensions/semantic-bundle/SemanticBundle.php" );
-include("$IP/extensions/intersection/DynamicPageList.php");
-require_once( "$IP/extensions/OpenID/OpenID.php" );
-
-?>
